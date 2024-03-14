0795f1884bab13427afb797e038aee3a6258668c
HaroruDa3 <harol.kock34@gmail.com>
2024-03-13 19:27:23 -0600

    finalizado

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent{
+export class NavbarComponent {
 
+  constructor() { }
+
+  closeNavbar(): void {
+    const navbarToggler = document.querySelector('.navbar-toggler') as HTMLElement;
+    const navbarCollapse = document.querySelector('.navbar-collapse') as HTMLElement;
+  
+    if (navbarToggler && navbarCollapse) {
+      const isTogglerCollapsed = navbarToggler.getAttribute('aria-expanded') === 'true';
+      if (isTogglerCollapsed) {
+        navbarToggler.click();
+      }
+    }
+  }
 }
